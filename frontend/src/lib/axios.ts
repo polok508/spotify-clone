3f06a637d0c0b434f5663d6247ae890657572d12
polok508 <polok508@gmail.com>
2025-06-18 21:04:11 +0300

    Решение проблемы с axios

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import .meta.env.MODE === "development" ? "http://localhost:5000/api" : "/",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://spotify-clone-6wol.onrender.com/api"
 });
